01fc5734176687b5ef37910fe7a6a4ac7db3d51a
Iván <ilv0003@alu.medac.es>
2024-01-16 19:03:25 +0100

    Royal Madrid

---
+++ Menu.js
@@ -0,0 +1,8 @@
+function Menu() {
+    var x = document.getElementsByTagName("nav")[0];
+    if (x.className === "") {
+        x.className = "responsive";
+    } else {
+        x.className = "";
+    }
+}
